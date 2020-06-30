49a0c12ba588d5b2225a55e421c502edb0930c00
Yusuke Ogata <yuyus9696@gmail.com>
2020-06-30 15:12:51 +0900

    re

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i;
+  int sum = 0;
+  for(i = 0; i <n; i++){
+    sum = sum + A[i] / x;
+  }
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if(p(mid)){
+      lb = mid;
+    }
+    else{
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
 
 
   return 0;
